83d83d355d03bcc767333b869b57a4c474343d10
Thibaut-Le-Goff <90935476+Thibaut-Le-Goff@users.noreply.github.com>
2022-09-15 20:44:00 +0200

    Add files via upload

--- src/main.rs
+++ src/main.rs
@@ -1,75 +1,52 @@
 mod runst;
 
 fn main() {
+    // Network initialisation:
+    println!("Initialisation du réseaux de neurones :");
+    println!("Le réseau :");
+    const LAYER: [usize; 4] = [3, 2, 6, 4];
+    println!("Le nombre de neurones de la première couche à la dernière :");
+    println!("{:?}\n", LAYER);
 
-    const LAYER: [usize; 3] = [2, 3, 1];
+    // The weights:
+    println!("Les poids :");
+    let matrix_weight_l0_1: Vec<f64> = runst::weight_init::he_uniform_dis(LAYER[0], LAYER[1]);
+    println!("La matrix des poids entre la couche 0(input) et 1 :");
+    println!("{:?}\n", matrix_weight_l0_1);
+   
+    let matrix_weight_l1_2: Vec<f64> = runst::weight_init::he_uniform_dis(LAYER[1], LAYER[2]);
+    println!("La matrix des poids entre la couche 1 et 2 :");
+    println!("{:?}\n", matrix_weight_l1_2);
 
-    let matrix: Vec<f64> = runst::weight_init::he_uniform_dis(LAYER[0], LAYER[1]);
-    //let vec6: Vec<f64> = vec![1.0; LAYER[0]];
-    let vec6: Vec<f64> = vec![2.0, 1.0, 1.0, 1.0, 1.0, 1.0];
-    let vec3: Vec<f64> = vec![2.0, 1.0, 1.0];
-    let vec2: Vec<f64> = vec![2.0, 1.0];
-    let vec1: Vec<f64> = vec![3.0];
+    let matrix_weight_l2_3: Vec<f64> = runst::weight_init::he_uniform_dis(LAYER[2], LAYER[3]);
+    println!("La matrix des poids entre la couche 2 et 3 :");
+    println!("{:?}\n", matrix_weight_l2_3);
 
-    println!("La matrise :");
-    println!("{:?}", matrix);
-
-    println!("Multipliée par le vecteur :");
-    println!("{:?}", vec6);
-
-    println!("Font :");
-    let mat6: Vec<f64> = runst::multiply(&matrix, &vec6);
-    println!("{:?}", mat6);
-
-    println!("Avec la fonction softmax, cela donne :");
-    let mat6: Vec<f64> = runst::activ_fun::softmax(&mat6);
-    println!("{:?}", mat6);
-
-
- 
-    println!("\n\n\nLa matrise :");
-    println!("{:?}", matrix);
-
-    println!("Multipliée par le vecteur :");
-    println!("{:?}", vec3);
-
-    println!("Font :");
-    let mat3: Vec<f64> = runst::multiply(&matrix, &vec3);
-    println!("{:?}", mat3);
-
-    println!("Avec la fonction softmax, cela donne :");
-    let mat3: Vec<f64> = runst::activ_fun::softmax(&mat3);
-    println!("{:?}", mat3);
+    // Creation of the tensor:
+    let tensor: [&Vec<f64>; 3] = [&matrix_weight_l0_1, &matrix_weight_l1_2, &matrix_weight_l2_3];
 
 
-
-    println!("\n\n\nLa matrise :");
-    println!("{:?}", matrix);
-
-    println!("Multipliée par le vecteur :");
-    println!("{:?}", vec2);
-
-    println!("Font :");
-    let mat2: Vec<f64> = runst::multiply(&matrix, &vec2);
-    println!("{:?}", mat2);
-
-    println!("Avec la fonction softmax, cela donne :");
-    let mat2: Vec<f64> = runst::activ_fun::softmax(&mat2);
-    println!("{:?}", mat2);
+    // Input layer:
+    println!("La couches des entrées, la numéros 0 :");
+    let vec_input: Vec<f64> = vec![1.0; LAYER[0]];
+    println!("La couche 0 a pour valeurs :");
+    println!("{:?}\n", vec_input);
 
 
-    
-    println!("\n\n\nLa matrise :");
-    println!("{:?}", matrix);
+    // Propagation
+    println!("Propagation des données d'entrée :");
+    println!("De la couche 0(input) à 1, à la couche 1, les données sont, à la sortie des neurones :");
+    let vec_l0_1: Vec<f64> = runst::multiply(tensor[0], &vec_input);
+    let vec_l1: Vec<f64> = runst::activ_fun::leaky_relu(&vec_l0_1);
+    println!("{:?}\n", vec_l1);
 
-    println!("Multipliée par le vecteur :");
-    println!("{:?}", vec1);
+    println!("De la couche 1 à 2, à la couche 2, les données sont, à la sortie des neurones :");
+    let vec_l1_2: Vec<f64> = runst::multiply(tensor[1], &vec_l1);
+    let vec_l2: Vec<f64> = runst::activ_fun::leaky_relu(&vec_l1_2);
+    println!("{:?}\n", vec_l2);
 
-    println!("Font :");
-    let mat1: Vec<f64> = runst::multiply(&matrix, &vec1);
-    println!("{:?}", mat1);
-
-    println!("Avec la fonction softmax, cela donne :");
-    let mat1: Vec<f64> = runst::activ_fun::softmax(&mat1);
-    println!("{:?}", mat1);
+    println!("De la couche 2 à 3(output), à la couche 3, les données sont, à la sortie des neurones :");
+    let vec_l2_3: Vec<f64> = runst::multiply(tensor[2], &vec_l2);
+    let vec_l3: Vec<f64> = runst::activ_fun::softmax(&vec_l2_3);
+    println!("{:?}\n", vec_l3);
 }
